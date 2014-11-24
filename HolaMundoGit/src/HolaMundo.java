8e330fdd8d2e8d5eec6ecf91d272862105a6238d
jose <jmlriego@gmail.com>
2014-11-24 13:30:21 +0100

    Commit para revertir un push

--- HolaMundoGit/src/HolaMundo.java
+++ HolaMundoGit/src/HolaMundo.java
@@ -5,7 +5,7 @@ public class HolaMundo {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("HolaMundoCambiado");
+		System.out.println("HolaMundoCambiadoParaRevertirPush");
 
 	}
 
