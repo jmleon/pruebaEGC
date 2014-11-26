42509f4f810afb005f328d03d2fe04a14cbdc58f
jose <jmlriego@gmail.com>
2014-11-26 11:18:03 +0100

    conflicto resuelto

--- HolaMundoGit/src/HolaMundo.java
+++ HolaMundoGit/src/HolaMundo.java
@@ -5,11 +5,9 @@ public class HolaMundo {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("HolaMundoCambiadoParaRevertirPush");
-=======
+
 		System.out.println("HolaMundoEditado en branch");
->>>>>>> test
+
 
 	}
 
